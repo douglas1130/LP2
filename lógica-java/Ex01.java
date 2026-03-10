b5040e2330d97bef1d68f5d97764a924491a5063
Douglas Eduardo Vieira Costa <douglasz.eduardo.3011@gmail.com>
2026-03-10 16:52:24 -0300

    Update Ex01.java

--- lógica-java/Ex01.java
+++ lógica-java/Ex01.java
@@ -1,1 +1,7 @@
-
+public class Ex01 {
+    public static void main (String[] args) {
+        System.out.println("Olá");
+        System.out.println("Eu estudo Ciência da Computação");
+        System.out.println("Na UERJ");        
+    }
+}
