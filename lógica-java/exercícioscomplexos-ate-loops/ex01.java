45450380f3b4f747691349006f708041852d20df
Douglas Eduardo Vieira Costa <douglasz.eduardo.3011@gmail.com>
2026-03-11 12:36:36 -0300

    Implement sum and print numbers from 1 to 100

--- lógica-java/exercícioscomplexos-ate-loops/ex01.java
+++ lógica-java/exercícioscomplexos-ate-loops/ex01.java
@@ -1,1 +1,17 @@
-
+public class Ex01 {
+    public static void main (String [] args) {
+        int soma = 0;
+        
+        for (int i = 1; i <= 100; i++) {
+            if (i != 100) {
+                System.out.print(i + " + ");
+            } else {
+                System.out.print(i);
+            }
+            
+            soma += i;
+        }
+        
+        System.out.print("\nSoma = " + soma);
+    }
+}
