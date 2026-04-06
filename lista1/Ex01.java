f21f820d55edd0dcc674d8bde33af027b717d263
Douglas Eduardo Vieira Costa <douglasz.eduardo.3011@gmail.com>
2026-04-06 19:35:39 -0300

    Update Ex01.java

--- lista1/Ex01.java
+++ lista1/Ex01.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite uma temperatura em grau Celsius: ");
+		double c = input.nextDouble();
+		
+		double f = (1.8 * c) + 32;
+		
+		System.out.println("Fahrenheit: " + f + " graus.");
+	}
+}
