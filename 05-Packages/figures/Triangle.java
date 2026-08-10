2c6116012397dd9f5052571328c4a87d2070b642
Douglas Eduardo Vieira Costa <douglasz.eduardo.3011@gmail.com>
2026-08-10 00:18:37 -0300

    Update Triangle.java

--- 05-Packages/figures/Triangle.java
+++ 05-Packages/figures/Triangle.java
@@ -3,14 +3,14 @@ package figures;
 import java.awt.*;
 
 public class Triangle {
-
     int x1, y1;
     int x2, y2;
     int x3, y3;
 
-    public Triangle(int x1, int y1,
-                    int x2, int y2,
-                    int x3, int y3) {
+    Color borderColor;
+    Color backgroundColor;
+
+    public Triangle(int x1, int y1, int x2, int y2, int x3, int y3, Color borderColor, Color backgroundColor) {
 
         this.x1 = x1;
         this.y1 = y1;
@@ -20,15 +20,13 @@ public class Triangle {
 
         this.x3 = x3;
         this.y3 = y3;
+
+        this.borderColor = borderColor;
+        this.backgroundColor = backgroundColor;
     }
 
     public void print() {
-        System.out.format(
-            "Triangulo com pontos (%d,%d), (%d,%d), (%d,%d).\n",
-            this.x1, this.y1,
-            this.x2, this.y2,
-            this.x3, this.y3
-        );
+        System.out.format("Triangulo com pontos (%d,%d), (%d,%d), (%d,%d).\n", this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
     }
 
     public void paint(Graphics g) {
@@ -39,6 +37,10 @@ public class Triangle {
 
         Polygon triangle = new Polygon(x, y, 3);
 
+        g2d.setColor(this.backgroundColor);
+        g2d.fillPolygon(triangle);
+
+        g2d.setColor(this.borderColor);
         g2d.drawPolygon(triangle);
     }
 }
