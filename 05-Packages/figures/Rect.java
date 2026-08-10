22df41033cadf956e2f8125bed35a0955a3146f1
Douglas Eduardo Vieira Costa <douglasz.eduardo.3011@gmail.com>
2026-08-10 00:00:32 -0300

    Update Rect.java

--- 05-Packages/figures/Rect.java
+++ 05-Packages/figures/Rect.java
@@ -6,20 +6,22 @@ public class Rect {
     int x, y;
     int w, h;
 
-    public Rect (int x, int y, int w, int h) {
+    public Rect(int x, int y, int w, int h) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
     }
 
-    public void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+    public void print() {
+        System.out.format(
+            "Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y
+        );
     }
 
-    public void paint (Graphics g) {
+    public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.drawRect(this.x, this.y, this.w, this.h);
     }
 }
